2e64c1a43fad67f04561f826e21fed0e763763d8
rupertberlin <ulrich.laitenberger@googlemail.com>
2022-04-22 12:20:49 +0200

    api route changed

--- backend/src/db/zodiacSigns.js
+++ backend/src/db/zodiacSigns.js
@@ -3,14 +3,17 @@ const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("horoscope.sqlite3");
 
 function getSignInformation(sign) {
-  return new Promise(function query(resolve) {
+  return new Promise((resolve) => {
     db.all(
       `SELECT * FROM signs WHERE sign="${
         sign[0].toUpperCase() + sign.slice(1)
       }"`,
-      function error(err, rows) {
+      (err, rows) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(rows);
         resolve(rows);
-        console.error(err.message);
       }
     );
   });
